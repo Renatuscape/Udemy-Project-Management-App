5ccd8761b66f2262349015e654821e41609efcee
Renatuscape <rrjonsberg@gmail.com>
2024-04-03 17:02:52 +0200

    Fixed typos in new Date and className that prevented module from working.

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,11 +1,11 @@
 export default function SelectedProject({project}) {
-    const formattedDate = newDate(project.dueDate).toLocaleDateString('en-UK', {
+    const formattedDate = new Date(project.dueDate).toLocaleDateString('en-UK', {
         year: 'numeric',
         month: 'short',
         day: 'numeric'
     });
 
-    return <div clasName="w-[35rem] mt-16">
+    return <div className="w-[35rem] mt-16">
         <header className="pb-4 mb-4 border-b-2 border-stone-300">
             <div className="flex items-center justify-between">
                 <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
