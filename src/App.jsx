042d9139f9cce41b603ffda5eadc553eecbd1af5
Renatuscape <rrjonsberg@gmail.com>
2024-04-03 16:59:09 +0200

    Added function for selecting project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
-import ProjectSidebar from "./components/ProjectSidebar";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   });
+
+  function handleSelectProject(id){
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id
+      };
+    })
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -36,7 +46,7 @@ function App() {
       }
       return {
         ...prevState,
-        selectedProjectId: undefined, //projectId,
+        selectedProjectId: projectId,
         projects: [...prevState.projects, newProject]
       }
     })
@@ -44,7 +54,11 @@ function App() {
 
   console.log(projectsState);
 
-  let content;
+  const selectedProject = projectsState.project.find(
+    (project) => project.id === projectsState.selectedProjectId
+    );
+  
+  let content = <SelectedProject project={selectedProject} />
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
@@ -54,7 +68,11 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectsSidebar
+      onStartAddProject={handleStartAddProject}
+      projects={projectsState.projects}
+      onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
