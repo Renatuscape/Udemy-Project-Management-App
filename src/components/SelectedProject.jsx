408f760a31d306e11656e079df3f75dd22a91854
Renatuscape <rrjonsberg@gmail.com>
2024-04-04 12:14:26 +0200

    Spread props for adding and deleting tasks.

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,6 +1,6 @@
 import Tasks from "./Tasks";
 
-export default function SelectedProject({project, onDelete}) {
+export default function SelectedProject({project, onDelete, onAddTask, onDeleteTask, tasks}) {
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-UK', {
         year: 'numeric',
         month: 'short',
@@ -18,6 +18,6 @@ export default function SelectedProject({project, onDelete}) {
             <p className="mb-4 text-stone-400">{formattedDate}</p>
             <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
         </header>
-        <Tasks />
+        <Tasks tasks={tasks} onAdd={onAddTask} onDelete={onDeleteTask}/>
     </div>
 }
